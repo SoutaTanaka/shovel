be5a4405ee369b3e31495206bfffa1833156698e
SoutaTanaka <tanakasouta@team-lab.com>
2025-01-25 17:22:01 +0900

    再起的な依存検知

--- lib/src/implementation/shovel_impl.dart
+++ lib/src/implementation/shovel_impl.dart
@@ -1,18 +1,27 @@
+import 'dart:collection';
+
 import '../../shovel.dart';
 
 import 'ground_impl.dart';
 
 class ShovelImpl implements Shovel {
   final Map<Type, Dig> _graph;
+  final Set<Type> _visited = {};
 
   ShovelImpl(this._graph);
 
   @override
   T dig<T>() {
+    if (_visited.contains(T)) {
+      throw StateError('Circular dependency detected for type $T');
+    }
+    _visited.add(T);
     final result = _graph[T];
     if (result == null) {
       throw StateError('No instance of type $T found');
     }
-    return result.call(this);
+    final instance = result.call(this);
+    _visited.remove(T);
+    return instance;
   }
 }
